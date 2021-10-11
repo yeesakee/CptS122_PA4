dcc8d3840e4c3ffbe407eff99b4116691eff989a
yeesakee <yeesakee99@gmail.com>
2021-10-11 09:53:10 -0700

    Added all diet headers in FitnessAppWrapper.h

--- PA4/FitnessAppWrapper.h
+++ PA4/FitnessAppWrapper.h
@@ -10,11 +10,31 @@
 
 class FitnessAppWrapper {
 public:
+	// starts the main application
+	void runApp(void);
+	// Displays nine menu options for the user to choose from
+	void displayMenu();
+
+	// Loads the daily diet plans from fileStream
+	void loadDailyPlanDiet(fstream& fileStream, DietPlan& plan);
+	// Loads the weekly diet plan given 
+	void loadWeeklyPlanDiet(fstream& fileStream, DietPlan weeklyPlan[]);
+	
+	// Loads the daily diet plan from input_diet
+	void displayDailyPlanDiet(fstream& input_diet);
+	// Loads the weekly diet plan
+	void displayWeeklyPlanDiet();
+
+	// Store the daily diet plan to output_diet
+	void storeDailyPlanDiet(fstream& output_diet);
+	// Store the weekly diet plan
+	void storeWeeklyPlanDiet();
+
 
 private:
 	ListDiet ld;
-	fstream input;
-	fstream output;
+	fstream input_diet;
+	fstream output_diet;
 };
 
 
